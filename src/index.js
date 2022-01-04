c089cfdd52d0510bddfda0fca982209fee809ecb
Spin4ester <bakibayev.r@gmail.com>
2022-01-04 18:23:58 +0600

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+
+  let numStr = Math.abs(n).toString();
+  let finalOutput = '';
   
+  for (let i = 0; i < numStr.length; i++) {
+  finalOutput = `${numStr[i]}${finalOutput}`;
+}
+  return finalOutput;
 }
